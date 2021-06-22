0497eca082e756f69d07d821262db94786c35b41
Tomasz Skiba <st.skiba.tomasz@gmail.com>
2021-06-22 16:02:52 +0200

    Add comment for delay

--- src/main/java/pl/skibahost/tasks/AbstractFileTask.java
+++ src/main/java/pl/skibahost/tasks/AbstractFileTask.java
@@ -35,6 +35,12 @@ public abstract class AbstractFileTask implements Runnable{
         }
     }
 
+    /**
+     * Alternative for Thread.sleep()
+     * For very short periods under 1ms for Linux and 10 ms for Windows it's not possible to suspend Thread.
+     * Suspension takes longer then that. There for is this method.
+     * @param delay delay in nano seconds
+     */
     protected void shortSleep(int delay){
         long currentNano = System.nanoTime();
         while (currentNano + delay >= System.nanoTime()){}
