ec7ec8c698da65212132fbfaea9c8044cc4970e2
Tomasz Skiba <st.skiba.tomasz@gmail.com>
2021-06-26 12:44:36 +0200

    Add Spring

--- src/main/java/pl/skibahost/Main.java
+++ src/main/java/pl/skibahost/Main.java
@@ -1,12 +1,16 @@
 package pl.skibahost;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import pl.skibahost.gui.Window;
 
+@SpringBootApplication
 public class Main {
 
     public static void main(String[] args){
         Window window = new Window();
         window.setVisible(true);
         window.pack();
+        SpringApplication.run(Main.class, args);
     }
 }
---
+++ src/main/java/pl/skibahost/rest/client/Client.java
@@ -0,0 +1,8 @@
+package pl.skibahost.rest.client;
+
+public class Client {
+
+    public static void main(String[] args){
+        System.out.println("dupa");
+    }
+}
