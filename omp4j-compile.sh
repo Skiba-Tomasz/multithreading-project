72ba6f54dfc93aa2a23ff9f4058601259472332c
Tomasz Skiba <st.skiba.tomasz@gmail.com>
2021-07-13 23:56:48 +0200

    Fix linux script

--- omp4j-compile.sh
+++ omp4j-compile.sh
@@ -1,3 +1,1 @@
-java -jar $(dirname "${BASH_SOURCE[0]}")/lib/omp4j-1.2.jar \
--d $(dirname "${BASH_SOURCE[0]}")/target/classes \
-$(dirname "${BASH_SOURCE[0]}")/src/main/java/pl/skibahost/impl/OmpImpl.java
+java -jar $(pwd)/lib/omp4j-1.2.jar -d $(pwd)/target/classes $(pwd)/src/main/java/pl/skibahost/impl/OmpImpl.java
